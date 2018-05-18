d7882110d5deff167f491263de5f0458901ced22
lennertMachiels <lennert.machiels@student.ehb.be>
2018-05-18 14:47:38 +0200

    translate

--- TEST/sketch.js
+++ TEST/sketch.js
@@ -1,22 +1,37 @@
-var xRadius, yRadius
-var randomShape;
+var xRadiusHead, yRadiusHead
+var xRadiusBody, yRadiusBody
+var randomHead;
+var randomBody;
 
 function setup() {
   createCanvas(640, 480);
-  randomShape = Math.round(random(1));
-  xRadius = random(30, 60);
-  yRadius = random(30, 50);
+  randomHead = Math.round(random(1));
+  xRadiusHead = random(30, 60);
+  yRadiusHead = random(30, 50);
   radiusCorners = random(0, 100);
+
+  xRadiusBody = random(60, 120);
+  yRadiusBody = random(60, 100);
 
 }
 
 function draw() {
-  if (randomShape) {
-
-    ellipse(80, 80, xRadius, yRadius);
+  if (randomHead) {
+    translate(0, -yRadiusHead/2);
+    ellipse(80, 80, xRadiusHead, yRadiusHead);
   } else {
     rectMode(CENTER);
-    rect(80, 80, xRadius, yRadius, radiusCorners);
+    translate(0, -yRadiusHead/2);
+    rect(80, 80, xRadiusHead, yRadiusHead, radiusCorners);
+  }
+
+  if (randomBody) {
+    translate(0, +yRadiusBody/2 + yRadiusHead/2);
+    ellipse(80, 80, xRadiusBody, yRadiusBody);
+  } else {
+    rectMode(CENTER);
+    translate(0, +yRadiusBody/2 + yRadiusHead/2);
+    rect(80, 80, xRadiusBody, yRadiusBody, radiusCorners);
   }
 
 }
