f2be5bbc96bef47f8c04c6811146ff4ed46057cf
Lennert Vanden Herrewegen <lennert.vanden.herrewegen@student.ehb.be>
2018-05-18 16:58:59 +0200

    dat.gui

--- TEST/sketch.js
+++ TEST/sketch.js
@@ -1,37 +1,49 @@
+var head;
 var xRadiusHead, yRadiusHead
 var xRadiusBody, yRadiusBody
 var randomHead;
 var randomBody;
 
 function setup() {
-  createCanvas(640, 480);
-  randomHead = Math.round(random(1));
-  xRadiusHead = random(30, 60);
-  yRadiusHead = random(30, 50);
-  radiusCorners = random(0, 100);
 
-  xRadiusBody = random(60, 120);
-  yRadiusBody = random(60, 100);
+
+  createCanvas(640, 480);
+  head = new headText();
+  var gui = new dat.GUI();
+  gui.add(head, 'xRadiusHead', 20, 60);
+  gui.add(head, 'yRadiusHead', 20, 60);
+  gui.add(head, 'shape', 0, 1);
+  gui.add(head, 'borderRadius', 0, 20);
 
 }
 
 function draw() {
-  if (randomHead) {
+  clear();
+  if (Math.round(head.shape)) {
     translate(0, -yRadiusHead / 2);
-    ellipse(80, 80, xRadiusHead, yRadiusHead);
+    ellipse(80, 80, head.xRadiusHead, head.yRadiusHead);
   } else {
     rectMode(CENTER);
     translate(0, -yRadiusHead / 2);
-    rect(80, 80, xRadiusHead, yRadiusHead, radiusCorners);
+    rect(80, 80, head.xRadiusHead, head.yRadiusHead, head.borderRadius);
   }
 
-  if (randomBody) {
-    translate(0, +yRadiusBody / 2 + yRadiusHead / 2);
-    ellipse(80, 80, xRadiusBody, yRadiusBody);
-  } else {
-    rectMode(CENTER);
-    translate(0, +yRadiusBody / 2 + yRadiusHead / 2);
-    rect(80, 80, xRadiusBody, yRadiusBody, radiusCorners);
-  }
+
+
+  // if (randomBody) {
+  //   translate(0, +yRadiusBody / 2 + yRadiusHead / 2);
+  //   ellipse(80, 80, xRadiusBody, yRadiusBody);
+  // } else {
+  //   rectMode(CENTER);
+  //   translate(0, +yRadiusBody / 2 + yRadiusHead / 2);
+  //   rect(80, 80, xRadiusBody, yRadiusBody, radiusCorners);
+  // }
 
 }
+
+function headText() {
+  this.xRadiusHead = 30;
+  this.yRadiusHead = 30;
+  this.shape = 1;
+  this.borderRadius = 0;
+};
