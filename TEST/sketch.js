9fdf314ee732ecde7465ca3edafe313dcf159f65
Lennert Vanden Herrewegen <lennert.vanden.herrewegen@student.ehb.be>
2018-05-22 10:56:12 +0200

    random

--- TEST/sketch.js
+++ TEST/sketch.js
@@ -7,19 +7,20 @@ var randomBody;
 function setup() {
 
 
-  createCanvas(640, 480);
+  createCanvas(1000, 1000);
   head = new headText();
   var gui = new dat.GUI();
-  gui.add(head, 'xRadiusHead', 20, 60);
-  gui.add(head, 'yRadiusHead', 20, 60);
+  gui.add(head, 'xRadiusHead', 20, 100);
+  gui.add(head, 'yRadiusHead', 20, 100);
   gui.add(head, 'shape');
   gui.add(head, 'borderRadius', 0, 20);
-  gui.add(head, 'hoeken', 0, 6);
+  gui.add(head, 'hoeken', 0, 40);
 
 }
 
 function draw() {
   clear();
+  rect(80, 140, 20, 120);
   if (Math.round(head.shape) == 0) {
     translate(0, -yRadiusHead / 2);
     ellipse(80, 80, head.xRadiusHead, head.yRadiusHead);
@@ -28,31 +29,44 @@ function draw() {
     translate(0, -yRadiusHead / 2);
     rect(80, 80, head.xRadiusHead, head.yRadiusHead, head.borderRadius);
   } else {
-    rectMode(CORNER);
     translate(0, -yRadiusHead / 2);
     polygon(80, 80, head.xRadiusHead, head.yRadiusHead, head.hoeken);
   }
+  rectMode(CENTER);
 
 
 
-  if (randomBody) {
-    translate(0, +yRadiusBody / 2 + yRadiusHead / 2);
-    ellipse(80, 80, xRadiusBody, yRadiusBody);
-  } else {
-    rectMode(CENTER);
-    translate(0, +yRadiusBody / 2 + yRadiusHead / 2);
-    rect(80, 80, xRadiusBody, yRadiusBody, radiusCorners);
-  }
+  // if (randomBody) {
+  //   translate(0, +yRadiusBody / 2 + yRadiusHead / 2);
+  //   ellipse(80, 80, xRadiusBody, yRadiusBody);
+  // } else {
+  //   rectMode(CENTER);
+  //   translate(0, +yRadiusBody / 2 + yRadiusHead / 2);
+  //   rect(80, 80, xRadiusBody, yRadiusBody, radiusCorners);
+  // }
 
 }
 
 function headText() {
-  this.xRadiusHead = 30;
-  this.yRadiusHead = 30;
-  this.shape = 0;
-  this.borderRadius = 0;
-  this.hoeken = 4
+  this.xRadiusHead = Math.random() * 80 + 1;
+  this.yRadiusHead = Math.random() * 80 + 1;
+  this.shape = Math.round(Math.random() * 3);
+  this.borderRadius = Math.round(Math.random() * 20 + 1);
+  this.hoeken = Math.round(Math.random() * 40 + 1);
+
+  console.log(this.xRadiusHead);
 };
+
+function randomShape(x, y, radiusX, radiusY, npoints) { //https://p5js.org/examples/form-regular-polygon.html
+  var angle = TWO_PI / npoints;
+  beginShape();
+  for (var a = 0; a < randomAantal; a++) {
+    var sx = x + cos(randomX[a]) * radiusX;
+    var sy = y + sin(randomY[a]) * radiusY;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
 
 
 function polygon(x, y, radiusX, radiusY, npoints) { //https://p5js.org/examples/form-regular-polygon.html
