ca3ce23272513b5ad84a58f2d7be23b895fd8b1e
lennertMachiels <lennert.machiels@student.ehb.be>
2018-05-22 11:32:16 +0200

    fjdztfvko

--- TEST/sketch.js
+++ TEST/sketch.js
@@ -1,31 +1,35 @@
-var head;
+var head, body;
 var xRadiusHead, yRadiusHead
 var xRadiusBody, yRadiusBody
 var randomHead;
 var randomBody;
+var Neck;
 
 function setup() {
 
-<<<<<<< HEAD
-  xRadiusBody = random(60, 120);
-  yRadiusBody = random(60, 100);
-=======
+  Neck = random(0, 20);
 
-  createCanvas(640, 480);
+  createCanvas(1000, 1000);
   head = new headText();
-  var gui = new dat.GUI();
-  gui.add(head, 'xRadiusHead', 20, 60);
-  gui.add(head, 'yRadiusHead', 20, 60);
-  gui.add(head, 'shape');
-  gui.add(head, 'borderRadius', 0, 20);
-  gui.add(head, 'hoeken', 0, 6);
+  var guiHead = new dat.GUI();
+  body = new bodyText();
+  var guiBody = new dat.GUI();
+  guiHead.add(head, 'xRadiusHead', 30, 100);
+  guiHead.add(head, 'yRadiusHead', 30, 100);
+  guiHead.add(head, 'shape');
+  guiHead.add(head, 'borderRadius', 0, 20);
+  guiHead.add(head, 'hoeken', 5, 40);
 
->>>>>>> 4e4a9db8c46ca5018fcef37ab7c31c0a677cc4b8
+  guiBody.add(body, 'xRadiusBody', 60, 150);
+  guiBody.add(body, 'yRadiusBody', 60, 150);
+  guiBody.add(body, 'shape');
+  guiBody.add(body, 'borderRadius', 0, 20);
+  guiBody.add(body, 'hoeken', 5, 40);
+
 }
 
-
-
 function draw() {
+    translate(100, 20);
   clear();
   if (Math.round(head.shape) == 0) {
     translate(0, -yRadiusHead / 2);
@@ -35,32 +39,45 @@ function draw() {
     translate(0, -yRadiusHead / 2);
     rect(80, 80, head.xRadiusHead, head.yRadiusHead, head.borderRadius);
   } else {
-    rectMode(CORNER);
+    rectMode(CENTER);
     translate(0, -yRadiusHead / 2);
-    polygon(80, 80, head.xRadiusHead, head.yRadiusHead, head.hoeken);
+    polygon(80, 80, head.xRadiusHead / 2, head.yRadiusHead / 2, head.hoeken);
   }
-<<<<<<< HEAD
-=======
+
+  translate(0, body.yRadiusBody / 2 + head.yRadiusHead / 2 + Neck);
 
 
+  if (Math.round(body.shape) == 0) {
+    translate(0, -yRadiusBody / 2);
+    ellipse(80, 80, body.xRadiusBody, body.yRadiusBody);
+  } else if (Math.round(body.shape) == 1) {
+    rectMode(CENTER);
+    translate(0, -yRadiusBody / 2);
+    rect(80, 80, body.xRadiusBody, body.yRadiusBody, body.borderRadius);
+  } else {
+    rectMode(CENTER);
+    translate(0, -yRadiusBody / 2);
+    polygon(80, 80, body.xRadiusBody / 2, body.yRadiusBody / 2, body.hoeken);
+  }
 
-  // if (randomBody) {
-  //   translate(0, +yRadiusBody / 2 + yRadiusHead / 2);
-  //   ellipse(80, 80, xRadiusBody, yRadiusBody);
-  // } else {
-  //   rectMode(CENTER);
-  //   translate(0, +yRadiusBody / 2 + yRadiusHead / 2);
-  //   rect(80, 80, xRadiusBody, yRadiusBody, radiusCorners);
-  // }
 
 }
 
 function headText() {
-  this.xRadiusHead = 30;
-  this.yRadiusHead = 30;
-  this.shape = 0;
-  this.borderRadius = 0;
-  this.hoeken = 4
+  this.xRadiusHead = random(40, 80);
+  this.yRadiusHead = random(40, 80);
+  this.shape = Math.round(random(0, 2));
+  this.borderRadius = random(0, 20);
+  this.hoeken = random(5, 40)
+};
+function bodyText() {
+  this.xRadiusBody = random(60, 150);
+  this.yRadiusBody = random(60, 150);
+  this.shape = Math.round(random(0, 2));
+  this.borderRadius = random(0, 20);
+  this.hoeken = random(5, 40)
+
+  console.log(this.shape);
 };
 
 
@@ -73,5 +90,4 @@ function polygon(x, y, radiusX, radiusY, npoints) { //https://p5js.org/examples/
     vertex(sx, sy);
   }
   endShape(CLOSE);
->>>>>>> 4e4a9db8c46ca5018fcef37ab7c31c0a677cc4b8
 }
